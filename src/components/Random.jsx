64705173478a5d4dbc15423c3b8b6d50ba75b775
student-MilanMandal <mandalmilan272@gmail.com>
2025-02-12 11:26:17 +0530

    add updated file

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -7,14 +7,18 @@ function Random() {
   const { gif, loading, fetchData } = useGif();
 
   return (
-    <div className="w-1/2  bg-green-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
+    <div className="md:w-1/2  bg-green-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px] sm:w-1/3">
       <h1 className=" mt-[15px] text-2xl underline uppercase font-bold ">
         A Random Gif
       </h1>
 
       {/* //spinner chechk true spinner false image */}
 
-      {loading ? <Spinner /> : <img src={gif} width={450} />}
+      {loading ? (
+        <Spinner />
+      ) : (
+        <img src={gif} className="sm:w-[150px] md:w-[450px]" />
+      )}
       <button
         onClick={() => fetchData()}
         className="w-10/12 bg-yellow-500 text-lg py-2 rounded-lg font-bold uppercase mb-[20px]"
--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -8,14 +8,18 @@ function Tag() {
   const { gif, loading, fetchData } = useGif(tag);
 
   return (
-    <div className="w-1/2  bg-blue-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
+    <div className="md:w-1/2  bg-blue-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px] sm:w-1/3">
       <h1 className=" mt-[15px] text-2xl underline uppercase font-bold ">
         Random {tag} Gif
       </h1>
 
       {/* //spinner chechk true spinner false image */}
 
-      {loading ? <Spinner /> : <img src={gif} width={450} />}
+      {loading ? (
+        <Spinner />
+      ) : (
+        <img src={gif} className="sm:w-[150px] md:w-[450px]" />
+      )}
 
       <input
         className="w-10/12 text-lg py-2 rounded-lg  mb-[3px] text-center"
